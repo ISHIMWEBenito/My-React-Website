8d28db01ab3cfffd834480ef5f5f23c66b456c72
Benito Ishimwe <benishimwe31@gmail.com>
2022-04-28 03:02:51 +0200

    Deploy my React-Website

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='services' element={<Services />} />
+        <Route path='products' element={<Products />} />
+        <Route path='sign-up' element={<SignUp />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
